2ca7f073150064514cbfcdd9797fe54d2d71b84f
zub4ever <31104589+zub4ever@users.noreply.github.com>
2021-10-08 13:05:09 -0500

    AJUSTES

--- certidoes.php
+++ certidoes.php
@@ -1,4 +1,4 @@
-<?php include("cabecalho.php");?>
+<?php include("cabecalho.php"); ?>
 <div class="jumbotron d-flex justify-content-center bg-light">
     <div class="text-black">
         <div class="ml-auto mt-auto mr-auto mb-auto">
@@ -10,53 +10,55 @@
     </div>
 </div>
 <div class="container">
-<hr class="featurette-divider "></br></br>
-  <div class="row" align="center">
-   <div class="col-lg-12 col-md-12">
-      <div class="single-service">
-        <div class="thumb">
-          <a href="documentos/certidoes/CRP.pdf" class="primary-btn header-btn text-uppercase mt-10">Certificado de Regularidade Previdenciária - CRP</a>                                   
+    <hr class="featurette-divider "></br></br>
+    <div class="row" align="center">
+        <div class="col-lg-12 col-md-12">
+            <div class="single-service">
+                <div class="thumb">
+                    <a href="documentos/certidoes/CRP.pdf" class="primary-btn header-btn text-uppercase mt-10">Certificado de Regularidade Previdenciária - CRP</a>                                   
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-12 col-md-12">
+            <div class="single-service">
+                <div class="thumb">
+                    <a href="documentos/certidoes/2021/crf.pdf" class="primary-btn header-btn text-uppercase mt-10">Certificado de Regularidade do FGTS - CRF</a>                                   
+                </div>
+            </div>
+        </div>
+        <!--
+      <div class="col-lg-12 col-md-12">
+        <div class="single-service">
+          <div class="thumb">
+            <a href="documentos/certidoes/cnd_uniao.pdf" class="primary-btn header-btn text-uppercase mt-10">CERTIDÃO NEGATIVA DE DÉBITOS  - CND UNIÃO</a>                               
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col-lg-12 col-md-12">
-      <div class="single-service">
-        <div class="thumb">
-          <a href="documentos/certidoes/FGTS.pdf" class="primary-btn header-btn text-uppercase mt-10">Certificado de Regularidade do FGTS - CRF</a>                                   
+        -->
+        <div class="col-lg-12 col-md-12">
+            <div class="single-service">
+                <div class="thumb">
+                    <a href="documentos/certidoes/2021/Certidao_negativa_pmrb.pdf" class="primary-btn header-btn text-uppercase mt-10">CERTIDÃO NEGATIVA DE DÉBITOS - CND PMRB Nº 55570/2021</a>                               
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col-lg-12 col-md-12">
-      <div class="single-service">
-        <div class="thumb">
-          <a href="documentos/certidoes/cnd_uniao.pdf" class="primary-btn header-btn text-uppercase mt-10">CERTIDÃO NEGATIVA DE DÉBITOS  - CND UNIÃO</a>                               
+        <div class="col-lg-12 col-md-12">
+            <div class="single-service">
+                <div class="thumb ">
+                    <a href="documentos/certidoes/2021/trabalhista.pdf" class="primary-btn header-btn text-uppercase mt-10">CERTIDÃO NEGATIVA DE DÉBITOS TRABALHISTAS Nº 31766864/2021</a>                                   
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col-lg-12 col-md-12">
-      <div class="single-service">
-        <div class="thumb">
-          <a href="documentos/certidoes/cnd_pmrb.pdf" class="primary-btn header-btn text-uppercase mt-10">CERTIDÃO NEGATIVA DE DÉBITOS - CND PMRB Nº 13459/2019</a>                               
+        <div class="col-lg-12 col-md-12">
+            <div class="single-service">
+                <div class="thumb">
+                    <a href="documentos/certidoes/2021/uniao.pdf" class="primary-btn header-btn text-uppercase mt-10">CERTIDÃO NEGATIVA DE DÉBITOS RELATIVOS AOS TRIBUTOS FEDERAIS E À DÍVIDA ATIVA</a>                               
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col-lg-12 col-md-12">
-      <div class="single-service">
-        <div class="thumb ">
-          <a href="documentos/certidoes/justica_do_trabalho.pdf" class="primary-btn header-btn text-uppercase mt-10">CERTIDÃO NEGATIVA DE DÉBITOS TRABALHISTAS Nº 171434592/2019</a>                                   
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-12 col-md-12">
-      <div class="single-service">
-        <div class="thumb">
-          <a href="documentos/certidoes/CND_TRIBUTOS_FEDERAIS_DV_ATIVA.pdf" class="primary-btn header-btn text-uppercase mt-10">CERTIDÃO NEGATIVA DE DÉBITOS RELATIVOS AOS TRIBUTOS FEDERAIS E À DÍVIDA ATIVA</a>                               
-        </div>
-      </div>
-    </div>
-  </div>     
+    </div>     
 
-  <hr class="featurette-divider"></br></br>
+    <hr class="featurette-divider"></br></br>
 </div>
 <?php include("rodape.php"); ?>
 
