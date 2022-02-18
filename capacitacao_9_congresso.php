e40b7f39674dca1e27bf4de8c4f7fb03fe42d137
zub4ever <sklpb@outlook.com>
2022-02-18 12:06:02 -0500

    Ajustes

---
+++ capacitacao_9_congresso.php
@@ -0,0 +1,250 @@
+<?php include("cabecalho.php"); ?>
+
+
+
+
+
+
+
+<div class="jumbotron d-flex justify-content-center bg-light">
+    <div class="text-black">
+        <div class="ml-auto mt-auto mr-auto mb-auto">
+            <div class="d-flex justify-content-center">
+                <img class="img-fluid" width="100px" src="img/logo2.png" alt="" style="width: 90px;">
+            </div>
+            <h2 class="text-left font-weight-bold text-uppercase py-1 text-white">Capacitações dos Conselheiros</h2>
+        </div>
+    </div>
+</div>
+<!-- partial:index.partial.html -->
+
+<div class="container">
+    
+    
+    <link rel="stylesheet" href="css/capacitacao/newCascadeStyleSheet.css">
+    <link rel="stylesheet" href="css/capacitacao/congresso.css">
+    <Conmain>
+
+        <h2>Cards Primary</h2>
+        <div class="card-group">
+            <div class="card card-primary text-center">
+                <img class="card-img-top" src="https://static3.avast.com/10001215/web/i/index2/for-home.jpg" alt="Card image cap">
+                <div class="card-body">
+                    
+                    <div class="card-title">Personal online protection everywhere you go</div>
+                    <p class="card-text">We protect your privacy and keep you safe online with Premium Security and our Virtual Private Network (VPN) that safeguards your personal data.</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-outline-secondary btn-icon-right">
+                        <span>HOME SECURITY
+                            <img src="https://static3.avast.com/1/web/i/v2/components/arrow-m-right-orange.png" height="24">
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="card card-primary text-center">
+                <img class="card-img-top" src="https://static3.avast.com/10001215/web/i/index2/for-partners.jpg" alt="Card image cap">
+                <div class="card-body">
+                    
+                    <div class="card-title">Simple solutions for business’s biggest security problems</div>
+                    <p class="card-text">We deliver complete endpoint and network security solutions designed for small and midsize businesses & IT service providers.</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-outline-secondary btn-icon-right">
+                        <span>BUSINESS SECURITY
+                            <img src="https://static3.avast.com/1/web/i/v2/components/arrow-m-right-orange.png" height="24">
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="card card-primary text-center">
+                <img class="card-img-top" src="https://static3.avast.com/10001215/web/i/index2/for-business.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <div class="card-title">Protecting the customers of global operators</div>
+                    <p class="card-text">We partner with the world’s leading mobile and broadband network operators to offer security, privacy, and digital parenting solutions.</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-outline-secondary btn-icon-right">
+                        <span>PARTNER SECURITY
+                            <img src="https://static3.avast.com/1/web/i/v2/components/arrow-m-right-orange.png" height="24">
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+      
+
+        <h2>Cards Secondary without body</h2>
+        <div class="card-deck">
+            <div class="card card-secondary">
+                <img class="card-img-top" src="https://via.placeholder.com/350x180.png?text=img" alt="Card image cap">
+                <div class="card-body">
+                    <div class="card-title">How to Choose the Best Password Manager</div>
+                </div>
+                <div class="card-footer text-right">
+                    <a href="#" class="btn btn-sm btn-link btn-icon-right">
+                        <span>LEARN MORE
+                            <img src="https://web-static.ff.int.avast.com/static3.avast.com/1/web/i/v2/components/arrow-s-right-orange.png" height="16">
+                        </span></a>
+                </div>
+            </div>
+            <div class="card card-secondary">
+                <img class="card-img-top" src="https://via.placeholder.com/350x180.png?text=img" alt="Card image cap">
+                <div class="card-body">
+                    <div class="card-title">How to Choose the Best Password Manager</div>
+                </div>
+                <div class="card-footer text-right">
+                    <a href="#" class="btn btn-sm btn-link btn-icon-right">
+                        <span>LEARN MORE
+                            <img src="https://web-static.ff.int.avast.com/static3.avast.com/1/web/i/v2/components/arrow-s-right-orange.png" height="16">
+                        </span></a>
+                </div>
+            </div>
+            <div class="card card-secondary">
+                <img class="card-img-top" src="https://via.placeholder.com/350x180.png?text=img" alt="Card image cap">
+                <div class="card-body">
+                    <div class="card-title">How to Choose the Best Password Manager</div>
+                </div>
+                <div class="card-footer text-right">
+                    <a href="#" class="btn btn-sm btn-link btn-icon-right">
+                        <span>LEARN MORE
+                            <img src="https://web-static.ff.int.avast.com/static3.avast.com/1/web/i/v2/components/arrow-s-right-orange.png" height="16">
+                        </span></a>
+                </div>
+            </div>
+            <div class="card card-secondary">
+                <img class="card-img-top" src="https://via.placeholder.com/350x180.png?text=img" alt="Card image cap">
+                <div class="card-body">
+                    <div class="card-title">How to Choose the Best Password Manager</div>
+                </div>
+                <div class="card-footer text-right">
+                    <a href="#" class="btn btn-sm btn-link btn-icon-right">
+                        <span>LEARN MORE
+                            <img src="https://web-static.ff.int.avast.com/static3.avast.com/1/web/i/v2/components/arrow-s-right-orange.png" height="16">
+                        </span></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="awards-container">
+            <h2>Cards with Awards</h2>
+            <div class="card-deck">
+                <div class="card card-awards text-center">
+                    <div class="card-img-top">
+                        <img src="https://static3.avast.com/10001215/web/i/awards-v12/white/awards-cnet.png" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <div class="card-title">The most downloaded software</div>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-sm btn-link-light btn-icon-right">
+                            <span>LEARN MORE
+                                <img src="https://web-static.ff.int.avast.com/static3.avast.com/1/web/i/v2/components/arrow-s-right-orange.png" height="16">
+                            </span></a>
+                    </div>
+                </div>
+                <div class="card card-awards text-center">
+                    <div class="card-img-top">
+                        <img src="https://static3.avast.com/10001215/web/i/awards-v12/logo-pcmag-2.png" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <div class="card-title">Best Free Anti-malware</div>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-sm btn-link-light btn-icon-right">
+                            <span>LEARN MORE
+                                <img src="https://web-static.ff.int.avast.com/static3.avast.com/1/web/i/v2/components/arrow-s-right-orange.png" height="16">
+                            </span></a>
+                    </div>
+                </div>
+                <div class="card card-awards text-center">
+                    <div class="card-img-top">
+                        <img src="https://static3.avast.com/10001215/web/i/awards-v12/white/awards-av-comparatives.png" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <div class="card-title">Antivirus with the lowest impact on PC performance</div>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-sm btn-link-light btn-icon-right">
+                            <span>LEARN MORE
+                                <img src="https://web-static.ff.int.avast.com/static3.avast.com/1/web/i/v2/components/arrow-s-right-orange.png" height="16">
+                            </span></a>
+                    </div>
+                </div>
+                <div class="card card-awards text-center">
+                    <div class="card-img-top">
+                        <img src="https://static3.avast.com/10001215/web/i/awards-v12/white/awards-softpedia.png" alt="Card image cap">
+                    </div>
+                    <div class="card-body">
+                        <div class="card-title">Official download partner</div>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-sm btn-link-light btn-icon-right">
+                            <span>LEARN MORE
+                                <img src="https://web-static.ff.int.avast.com/static3.avast.com/1/web/i/v2/components/arrow-s-right-orange.png" height="16">
+                            </span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <h2>Accordion</h2>
+        <div class="accordion-container">
+            <div class="accordion">
+                <div class="card card-accordion">
+                    <div class="card-header" id="headingOne">
+                        <span>What are the system requirements for Avast Premium Security?</span>
+                        <a href="#collapseOne" class="icon-btn" data-toggle="collapse"></a>
+                    </div>
+
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-accordion">
+                    <div class="card-header" id="headingTwo">
+                        <span>What's the difference between Avast Premium Security (Single-Device) and Avast Premium Security (Multi-Device) subscriptions?</span>
+                        <a href="#collapseTwo" class="icon-btn" data-toggle="collapse"></a>
+                    </div>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-accordion">
+                    <div class="card-header" id="headingThree">
+                        <span>Can I change the 10 devices activated with my Avast Premium Security (Multi-Device) subscription?</span>
+                        <a href="#collapseThree" class="icon-btn" data-toggle="collapse"></a>
+                    </div>
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-accordion">
+                    <div class="card-header" id="headingFour">
+                        <span>Is it necessary to uninstall my previous Avast Antivirus version before upgrading to Avast Premium Security on my PC?</span>
+                        <a href="#collapseFour" class="icon-btn" data-toggle="collapse"></a>
+                    </div>
+                    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Conmain>
+</div>
+<!-- partial -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/popper.min.js'></script>
+<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'></script>
+
+<?php include("rodape.php"); ?>
---
+++ capacitaoes.php
@@ -0,0 +1,46 @@
+<?php include("cabecalho.php"); ?>
+
+<html lang="en" >
+    <head>
+        <meta charset="UTF-8">
+        
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!------ Include the above in your HEAD tag ---------->
+
+       
+        <link rel="stylesheet" href="css/capacitacoes/style.css">
+
+    </head>
+    <body>
+        <br><br>
+        <!-- partial:index.partial.html -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card card-1">
+                        <h3>9º Congresso Brasileiro de Conselheiros de RPPS`s</h3>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card-2">
+                        <h3>4° Congresso Nacional da ABIPEM Goiânia 2021  </h3>
+
+
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card-3">
+                        <h3>3° Congresso Brasileiro de Investimento dos RPPS</h3>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <!-- partial -->
+    </body>
+</html>
+
+<?php include("rodape.php"); ?>
